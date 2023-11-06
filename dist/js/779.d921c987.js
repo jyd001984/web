a1fbe2f3a00e7b2cbf509e5d4ff907eb9bc1fb23
jyd001984 <142872528+jyd001984@users.noreply.github.com>
2023-11-06 20:55:00 +0800

    Delete dist directory

--- dist/js/779.d921c987.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkbadmintion"]=self["webpackChunkbadmintion"]||[]).push([[779],{9779:function(t,e,n){n.r(e),n.d(e,{default:function(){return l}});var i=function(){var t=this,e=t._self._c;return e("div",[e("el-empty",{attrs:{"image-size":200}},[e("el-button",{attrs:{type:"primary"},on:{click:t.back}},[t._v("返回首页")])],1)],1)},r=[],u=(n(7658),{methods:{back(){this.$router.push("/")}}}),s=u,a=n(1001),c=(0,a.Z)(s,i,r,!1,null,"ef1554d0",null),l=c.exports}}]);
-//# sourceMappingURL=779.d921c987.js.map
